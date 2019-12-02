c1fdf2b8efb808a27058577ab04f826e55d914c5
KApilBD <kapilbaraskar91@gmail.com>
2019-12-02 23:06:11 +0530

    Added React Context in application

--- blog/App.js
+++ blog/App.js
@@ -1,16 +1,24 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator({
   Index: IndexScreen
-},{
+}, {
   initialRouteName: 'Index',
-  defaultNavigationOptions:{
-    title:'Blogs',
+  defaultNavigationOptions: {
+    title: 'Blogs',
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (<BlogProvider>
+    <App />
+  </BlogProvider>);
+};
 
