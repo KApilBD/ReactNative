938538f8694ff31dca48c21b8cb018f8e91a23fd
KApilBD <kapilbaraskar91@gmail.com>
2019-12-24 00:01:49 +0530

    Tracking current location

--- tracks/src/context/locationContext.js
+++ tracks/src/context/locationContext.js
@@ -2,7 +2,8 @@ import createDataCOntext from './createDataContext';
 
 const locationReducer = (state, action) => {
     switch (action.type) {
-
+        case "add_current_location":
+            return { ...state, currentLocation: action.payload };
         default:
             state;
     }
@@ -16,12 +17,12 @@ const stopRecording = dispatch => () => {
 
 };
 
-const addLocation = dispatch => () => {
-
+const addLocation = dispatch => (location) => {
+    dispatch({ type: "add_current_location", payload: location });
 };
 
 export const { Context, Provider } = createDataCOntext(
     locationReducer,
-    { startRecording, startRecording, addLocation },
+    { startRecording, startRecording, stopRecording, addLocation },
     { recording: false, location: [], currentLocation: null }
 );
